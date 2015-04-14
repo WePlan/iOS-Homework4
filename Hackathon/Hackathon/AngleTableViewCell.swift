e248bb2c0e1ebcc89ecc4563e90b1bf066ca8041
chenkanck <chemkanck1990@gmail.com>
2015-04-14 18:53:31 -0400

    new start point

---
+++ Hackathon/Hackathon/AngleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AngleTableViewCell.swift
+//  Hackathon
+//
+//  Created by Kan Chen on 4/14/15.
+//  Copyright (c) 2015 NYU-poly. All rights reserved.
+//
+
+import UIKit
+
+class AngleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
